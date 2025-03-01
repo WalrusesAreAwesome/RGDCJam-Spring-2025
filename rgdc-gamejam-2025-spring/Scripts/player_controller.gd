ea34087f5906433469e8e17adbbd6acfe2981c48
WalrusesAreAwesome <rysdam.jon@gmail.com>
2025-03-01 14:57:02 -0500

    Better Player Sprites

--- rgdc-gamejam-2025-spring/Scripts/player_controller.gd
+++ rgdc-gamejam-2025-spring/Scripts/player_controller.gd
@@ -1,5 +1,7 @@
 class_name player_controller extends CharacterBody2D
 
+@onready var _animated_sprite = $AnimatedSprite2D
+
 var isPouncing = false
 var pounceVelocity = Vector2.ZERO
 @export var walkSpeed = 50
@@ -20,22 +22,31 @@ func _process(delta: float) -> void:
 func _physics_process(delta: float) -> void:
 	if (!isPouncing and Input.is_action_just_pressed("pounce")):
 		update_pounce()
-	update_position(delta)
+	update_velocity(delta)
+	flip_sprite_right_way()
 	var collided = move_and_slide()
 	if (isPouncing and collided):
 		isPouncing = false
+		_animated_sprite.play("walkin")
 		bounceTimer = 0.15
+
+func flip_sprite_right_way():
+	if (velocity.x < 0):
+		_animated_sprite.flip_h = true
+	if (velocity.x > 0):
+		_animated_sprite.flip_h = false
 
 
 func update_pounce():
 	isPouncing = true
+	_animated_sprite.play("pounce")
 	var mouse_pos = get_node("Camera2D").get_global_mouse_position()
 	pounceVelocity = mouse_pos - position
 	pounceVelocity = pounceVelocity.normalized()
 	pounceVelocity *= pounceSpeed
 
 
-func update_position(delta):
+func update_velocity(delta):
 	if (isPouncing):
 		velocity = pounceVelocity
 		return
@@ -43,6 +54,8 @@ func update_position(delta):
 	if (bounceTimer > 0):
 		bounceTimer -= delta
 		velocity = -pounceVelocity/5
+		if (bounceTimer <= 0):
+			_animated_sprite.stop()
 		return
 	
 	# Not pouncing
@@ -55,5 +68,10 @@ func update_position(delta):
 		frameVelocity += Vector2(0, -1)
 	if (Input.is_action_pressed("down")):
 		frameVelocity += Vector2(0, 1)
+	
+	if (frameVelocity.length_squared() > 0):
+		_animated_sprite.play("walkin")
+	else:
+		_animated_sprite.stop()
 	
 	velocity = frameVelocity.normalized() * walkSpeed
