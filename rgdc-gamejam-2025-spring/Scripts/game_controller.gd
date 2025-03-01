60f2de7e73aa0755e0b260532635fc39925727d2
Rat Queen <eggcelent4@gmail.com>
2025-03-01 11:36:49 -0500

    add shortcut for Jon test scene

--- rgdc-gamejam-2025-spring/Scripts/game_controller.gd
+++ rgdc-gamejam-2025-spring/Scripts/game_controller.gd
@@ -4,12 +4,26 @@ extends Node
 var main_menu_path = "res://Scenes/MainMenu.tscn"
 var game_path = "res://Scenes/Game.tscn"
 var pause_menu_path = "res://Scenes/"
+var jon_test_path = "res://Scenes/JonTestScene.tscn"
 
 
 
 # when app starts, create main menu
 func _ready() -> void:
 	open_main_menu()
+
+
+# update func
+func _process(delta: float) -> void:
+	jon_test_scene()
+
+
+
+## test scene shortcuts ##
+
+func jon_test_scene() -> void:
+	if Input.is_action_just_pressed("jon_test_scene"):
+		set_child_from_path(jon_test_path)
 
 
 
