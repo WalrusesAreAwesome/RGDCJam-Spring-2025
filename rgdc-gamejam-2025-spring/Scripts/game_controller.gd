4e47a87cc8340c0fe3e8c5277020b7e41b1036e5
Rat Queen <eggcelent4@gmail.com>
2025-03-01 11:24:02 -0500

    created main menu and fonts

--- rgdc-gamejam-2025-spring/Scripts/game_controller.gd
+++ rgdc-gamejam-2025-spring/Scripts/game_controller.gd
@@ -1,11 +1,45 @@
 extends Node
 
 
-# Called when the node enters the scene tree for the first time.
+var main_menu_path = "res://Scenes/MainMenu.tscn"
+var game_path = "res://Scenes/Game.tscn"
+var pause_menu_path = "res://Scenes/"
+
+
+
+# when app starts, create main menu
 func _ready() -> void:
-	pass # Replace with function body.
+	open_main_menu()
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+
+## handling scenes ##
+
+
+# open main menu scene
+func open_main_menu():
+	set_child_from_path(main_menu_path)
+
+
+
+# open game scene
+func open_game():
+	set_child_from_path(main_menu_path)
+
+
+
+# pause game
+func open_pause_menu():
+	set_child_from_path(main_menu_path)
+
+
+
+# load scene and set as child from given path
+func set_child_from_path(path: String):
+	var scene = load(path).instantiate()
+	# remove all current children
+	for child in get_children():
+		child.queue_free()
+		remove_child(child)
+	# add new scene as child
+	add_child(scene)
