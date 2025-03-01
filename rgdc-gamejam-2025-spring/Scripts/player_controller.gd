4bfe73f1a4274d7eea8db1b85a35e01187fbfd63
WalrusesAreAwesome <rysdam.jon@gmail.com>
2025-03-01 12:44:09 -0500

    Pounce and WASD

--- rgdc-gamejam-2025-spring/Scripts/player_controller.gd
+++ rgdc-gamejam-2025-spring/Scripts/player_controller.gd
@@ -3,6 +3,7 @@ class_name player_controller extends CharacterBody2D
 var isPouncing = false
 var pounceVelocity = Vector2.ZERO
 @export var walkSpeed = 50
+@export var pounceSpeed = 250
 
 
 # Called when the node enters the scene tree for the first time.
@@ -17,14 +18,28 @@ func _process(delta: float) -> void:
 
 
 func _physics_process(delta: float) -> void:
+	if (!isPouncing and Input.is_action_just_pressed("pounce")):
+		update_pounce()
 	update_position()
-	move_and_slide()
+	var collided = move_and_slide()
+	if (isPouncing and collided):
+		isPouncing = false
+
+
+func update_pounce():
+	isPouncing = true
+	var mouse_pos = get_node("Camera2D").get_global_mouse_position()
+	print(mouse_pos)
+	print(position)
+	pounceVelocity = mouse_pos - position
+	pounceVelocity = pounceVelocity.normalized()
+	pounceVelocity *= pounceSpeed
 
 
 func update_position():
 	if (isPouncing):
-		self.position += pounceVelocity
-		pass
+		velocity = pounceVelocity
+		return
 	
 	# Not pouncing
 	var frameVelocity = Vector2.ZERO
