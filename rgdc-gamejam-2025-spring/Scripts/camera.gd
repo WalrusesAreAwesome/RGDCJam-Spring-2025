817784b84408d84d347c2d636a3fca3cf476bc71
WalrusesAreAwesome <rysdam.jon@gmail.com>
2025-03-01 15:02:13 -0500

    Camera

---
+++ rgdc-gamejam-2025-spring/Scripts/camera.gd
@@ -0,0 +1,16 @@
+extends Camera2D
+
+var viewport
+var zoomFactor
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	viewport = get_viewport().size
+	zoomFactor = min( viewport.x/12.0, viewport.y / 7.5  )
+	set_zoom( Vector2(zoomFactor, zoomFactor) )
+	pass
