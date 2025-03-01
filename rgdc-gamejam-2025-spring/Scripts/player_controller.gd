8efef05a7d29e8226244bce2ada2d12818d77af4
WalrusesAreAwesome <rysdam.jon@gmail.com>
2025-03-01 11:26:43 -0500

    Player Movement

---
+++ rgdc-gamejam-2025-spring/Scripts/player_controller.gd
@@ -0,0 +1,35 @@
+extends Node
+
+var isPouncing = false
+var pounceVelocity = Vector2.ZERO
+var walkSpeed = 50
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	update_position(delta)
+	pass
+
+
+func update_position(delta):
+	if (isPouncing):
+		self.position += pounceVelocity * delta
+		pass
+	
+	# Not pouncing
+	var frameVelocity = Vector2.ZERO
+	if (Input.is_action_pressed("left")):
+		frameVelocity += Vector2(-1, 0)
+	if (Input.is_action_pressed("right")):
+		frameVelocity += Vector2(1, 0)
+	if (Input.is_action_pressed("up")):
+		frameVelocity += Vector2(-1, 0)
+	if (Input.is_action_pressed("down")):
+		frameVelocity += Vector2(1, 0)
+	
+	self.position += frameVelocity.normalized() * walkSpeed * delta
